24419098815fa5d5be2f256d8086e66487f0cd5d
Gowtham  H D <gowthambhat793@gmail.com>
2020-10-19 20:12:29 +0530

    POST and PUT methods are created

---
+++ models/treeSchema.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const treeSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 25
+    },
+    ScientificName: {
+        type: String,
+        minlength: 3,
+        maxlength: 25
+    },
+    medicalUse: {
+        type: String,
+        minlength: 3,
+        maxlength: 250
+    },
+    commonName: {
+        type: String,
+        minlength: 3,
+        maxlength: 25,
+        required: true
+    },
+    availabilityRegion: {
+        type: String,
+        minlength: 3,
+        maxlength: 10
+    }
+});
+
+const Tree = mongoose.model('tree', treeSchema);
+
+module.exports = Tree;
--- routes/tree.js
+++ routes/tree.js
@@ -1,9 +1,53 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const router = express.Router();
+const Tree = require('../models/treeSchema');
 
-router.get('/list', (req, res) => {
-    res.send('tree list here / get request');
+router.get('/list', async (req, res) => {
+    // try {
+    //     const trees = await Tree.find().sort('name');
+    //     res.send(trees);
+    // } catch (error) {
+    //     res.status(501).send('something went wrong');
+    //     console.log(error);
+
+    // }
+    try {
+        const trees = await Tree.find().sort('name');
+        console.log(trees);
+
+        res.render('treeList', { trees: trees })
+    } catch (error) {
+        res.send('something went wrong');
+    }
 });
+router.post('/list', async (req, res) => {
+    try {
+        let trees = new Tree({
+            name: req.body.name,
+            commonName: req.body.commonName
+        });
+        trees = await trees.save();
+        res.send(trees);
+    } catch (error) {
+        res.status(501).send('something went wrong');
+        console.log(error);
 
+    }
+});
+router.put('/list/:id', async (req, res) => {
+    try {
+        console.log(req.params.id);
+
+        const tree = await Tree.findByIdAndUpdate(req.params.id, {
+            name: req.body.name,
+            commonName: req.body.commonName
+        }, { new: true });
+        res.send(tree);
+    } catch (error) {
+        console.log(error);
+        res.status(501).send('something went wrong');
+    }
+
+});
 module.exports = router;
